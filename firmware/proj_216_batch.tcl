91f4e86c154d302dd2aaba899684eb32ac66aa93
Giuseppe Di Guglielmo <gdg@fnal.gov>
2023-10-04 08:05:36 -0700

    Add bistream for zcu216 without NN, just an ILA

--- firmware/proj_216_batch.tcl
+++ firmware/proj_216_batch.tcl
@@ -1,5 +1,16 @@
 source proj_216.tcl
 
+# === BEGIN: DEBUG ===============================================================
+# Open block diagram
+open_bd_design {${proj_dir}/top_216.srcs/sources_1/bd/d_1/d_1.bd}
+
+set_property HDL_ATTRIBUTE.DEBUG true [get_bd_intf_nets {axis_readout_v2_0_m1_axis}]
+apply_bd_automation -rule xilinx.com:bd_rule:debug -dict [list \
+    [get_bd_intf_nets axis_readout_v2_0_m1_axis] \
+    {AXIS_SIGNALS "Data and Trigger" CLK_SRC "/usp_rf_data_converter_0/clk_adc2" SYSTEM_ILA "Auto" APC_EN "0" } \
+]
+# === END: DEBUG ===============================================================
+
 reset_run impl_1
 reset_run synth_1
 launch_runs impl_1 -to_step write_bitstream -jobs 20
