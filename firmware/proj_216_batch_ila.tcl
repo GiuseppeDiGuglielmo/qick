58a52fbe4705fd7ffefb1c7e387f1a079eeed276
Giuseppe Di Guglielmo <gdg@fnal.gov>
2024-01-03 20:51:03 -0800

    Debug latest QICK without NN

--- firmware/proj_216_batch_ila.tcl
+++ firmware/proj_216_batch_ila.tcl
@@ -16,6 +16,12 @@ set_property HDL_ATTRIBUTE.DEBUG true [get_bd_nets {vect2bits_16_0_dout14 }]
 set_property -dict [list CONFIG.C_BRAM_CNT {1} CONFIG.C_MON_TYPE {MIX}] [get_bd_cells system_ila_0]
 connect_bd_net [get_bd_pins system_ila_0/probe0] [get_bd_pins vect2bits_16_0/dout14]
 
+set_property HDL_ATTRIBUTE.DEBUG true [get_bd_intf_nets {axis_broadcaster_0_M00_AXIS}]
+apply_bd_automation -rule xilinx.com:bd_rule:debug -dict [list \
+    [get_bd_intf_nets axis_broadcaster_0_M00_AXIS] \
+    {AXIS_SIGNALS "Data and Trigger" CLK_SRC "/usp_rf_data_converter_0/clk_adc2" SYSTEM_ILA "Auto" APC_EN "0" } \
+]
+
 # Validation
 validate_bd_design
 
