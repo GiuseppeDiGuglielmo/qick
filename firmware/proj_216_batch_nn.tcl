66ee8e332365b3814c1b506ace5784a2c249d9c8
Giuseppe Di Guglielmo <gdg@fnal.gov>
2024-03-08 11:09:33 -0800

    Enable synthesis for lightweight QICK

--- firmware/proj_216_batch_nn.tcl
+++ firmware/proj_216_batch_nn.tcl
@@ -6,9 +6,9 @@ open_bd_design {${proj_dir}/top_216.srcs/sources_1/bd/d_1/d_1.bd}
 # Simplify the QICK design (faster synthesis)
 source delete_ips_216.tcl
 
-#reset_run impl_1
-#launch_runs impl_1 -to_step write_bitstream -jobs 20
-#wait_on_run -timeout 360 impl_1
-#
-#open_run impl_1
-#report_utilization -file util_216.rpt -hierarchical -hierarchical_percentages
+reset_run impl_1
+launch_runs impl_1 -to_step write_bitstream -jobs 20
+wait_on_run -timeout 360 impl_1
+
+open_run impl_1
+report_utilization -file util_216.rpt -hierarchical -hierarchical_percentages
