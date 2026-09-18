c3616c30587380a344f1b3b18be3db6109e4094c
Giuseppe Di Guglielmo <gdg@fnal.gov>
2026-09-18 08:26:18 -0700

    Add Makefile build flow for ZCU216
    Add a Makefile with targets to create, build and open the top_216 and
    top_216_orig Vivado projects. proj_216.mk holds the per-project
    targets. proj_216_orig.tcl runs batch synthesis and implementation
    through bitstream generation and writes a utilization report.
    
    Add clean and distclean targets. distclean also removes the generated
    top_216* project directories and package files.
    
    Targets for ila, nn and packaging are kept as comments until their
    scripts exist.

---
+++ firmware/proj_216.mk
@@ -0,0 +1,115 @@
+# Create original Vivado project from proj_216.tcl (no implementation run)
+syn-zcu216:
+	vivado -source proj_216.tcl
+.PHONY: syn-zcu216
+
+# Create original Vivado project and run implementation (batch/GUI mode)
+syn-zcu216-orig: check_license
+ifeq ($(GUI),1)
+	vivado -mode gui -source proj_216_orig.tcl
+else
+	bash -c 'TIMEFORMAT="Elapsed time: %0lR"; time vivado -mode batch -source proj_216_orig.tcl'
+endif
+.PHONY: syn-zcu216-orig
+
+# Disabled: proj_216_ila.tcl does not exist in this repo yet
+## Batch/GUI mode: create Vivado project with ILAs and run implementation
+#syn-zcu216-ila: check_license
+#ifeq ($(GUI),1)
+#	vivado -mode gui -source proj_216_ila.tcl
+#else
+#	bash -c 'TIMEFORMAT="Elapsed time: %0lR"; time vivado -mode batch -source proj_216_ila.tcl'
+#endif
+#.PHONY: syn-zcu216-ila
+
+# Disabled: proj_216_nn.tcl does not exist in this repo yet
+## Batch/GUI mode: create Vivado project with NN (BRAMs + AXI-lite), ILAs, and run implementation
+#syn-zcu216-nn: check_license
+#	@rm -rf ip_local/*
+#ifeq ($(GUI),1)
+#	vivado -mode gui -source proj_216_nn.tcl
+#else
+#	bash -c 'TIMEFORMAT="Elapsed time: %0lR"; time vivado -mode batch -source proj_216_nn.tcl'
+#endif
+#.PHONY: syn-zcu216-nn
+
+# Open GUI of the top_216 Vivado project created by syn-zcu216
+gui-zcu216:
+	vivado top_216/top_216.xpr
+.PHONY: gui-zcu216
+
+# Open GUI of the top_216_orig Vivado project created by syn-zcu216-orig
+gui-zcu216-orig:
+	vivado top_216_orig/top_216.xpr
+.PHONY: gui-zcu216-orig
+
+# Disabled: top_216_ila/ is only created by syn-zcu216-ila, which is disabled above
+## Open GUI of the latest Vivado project
+#gui-zcu216-ila:
+#	vivado top_216_ila/top_216.xpr
+#.PHONY: gui-zcu216-ila
+
+# Disabled: top_216_nn/ is only created by syn-zcu216-nn, which is disabled above
+## Open GUI of the latest Vivado project
+#gui-zcu216-nn:
+#	vivado top_216_nn/top_216.xpr
+#.PHONY: gui-zcu216-nn
+
+# Disabled: package.sh does not exist in this repo yet
+## Package BIT, HWH, and LTX files
+#package-zcu216-orig:
+#	@./package.sh \
+#		top_216 \
+#		d_1 \
+#		top_216_orig \
+#		qick_216_orig
+#.PHONY: package-zcu216-orig
+#
+## Package BIT, HWH, and LTX files
+#package-zcu216-ila:
+#	@./package.sh \
+#		top_216 \
+#		d_1 \
+#		top_216_ila \
+#		qick_216_ila
+#.PHONY: package-zcu216-ila
+#
+## Package BIT, HWH, and LTX files
+#package-zcu216-nn:
+#	@./package.sh \
+#		top_216 \
+#		d_1 \
+#		top_216_nn \
+#		qick_216_nn
+#.PHONY: package-zcu216-nn
+
+# Disabled: copy.sh does not exist in this repo yet
+## Copy package remotely
+#copy-zcu216:
+#	@./copy.sh \
+#		xilinx@rfsoc216-ml01.dhcp.fnal.gov:~/jupyter_notebooks/qick_dev/qick_ml/216/ml-dev \
+#		qick_216 \
+#		quantum2023.
+#.PHONY: copy-zcu216
+
+# Remove top_216 project directory created by syn-zcu216
+distclean-zcu216:
+	@rm -rf top_216
+.PHONY: distclean-zcu216
+
+# Remove top_216_orig project directory created by syn-zcu216-orig
+distclean-zcu216-orig:
+	@rm -rf top_216_orig
+.PHONY: distclean-zcu216-orig
+
+# Disabled: top_216_ila/ is only created by syn-zcu216-ila, which is disabled above
+## Remove top_216_ila project directory
+#distclean-zcu216-ila:
+#	@rm -rf top_216_ila
+#.PHONY: distclean-zcu216-ila
+
+# Disabled: top_216_nn/ is only created by syn-zcu216-nn, which is disabled above
+## Remove top_216_nn project directory
+#distclean-zcu216-nn:
+#	@rm -rf top_216_nn
+#.PHONY: distclean-zcu216-nn
